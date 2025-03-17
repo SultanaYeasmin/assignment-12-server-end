a8d9e27679f34e1801c0c444f1a6bc02fcd170c4
Sultana Yeasmin <EO005092@bifpcl.net>
2025-03-17 15:16:48 +0600

    user-data-added-to-database

---
+++ index.js
@@ -0,0 +1,67 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+
+const port = process.env.PORT || 5000;
+const app = express();
+
+//middleware
+app.use(express.json());
+app.use(cors());
+
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.db_username}:${process.env.db_password}@cluster0.g4p4k.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+    const database = client.db("parcelDB");
+    const usersCollection = database.collection("users")
+
+    //saving user data to db
+    app.post('/users/:email', async (req, res) => {
+      const email = req.params.email;
+      const userData = req.body;
+      const query = { email };
+      const isExist = await usersCollection.findOne(query);
+      if (isExist) {
+        return res.send(isExist)
+      }
+      const result = await usersCollection.insertOne(userData)
+      res.send(result);
+    })
+
+
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get('/', async (req, res) => {
+  res.send("Transito-Parcel Management System!")
+})
+
+app.listen(port, () => {
+  console.log("Port is running on port no:", port)
+})
+
+
